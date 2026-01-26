07b07e5ce96337d02ef6c6c0df354fc282e19ecf
RanaOzair27 <ranaozair27@gmail.com>
2026-01-26 07:17:20 +0500

    Added the Rule model for the rule-engine

--- rule-engine/src/main/java/com/learning/rule_engine/RuleEngineApplication.java
+++ rule-engine/src/main/java/com/learning/rule_engine/RuleEngineApplication.java
@@ -2,8 +2,10 @@ package com.learning.rule_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RuleEngineApplication {
 
 	public static void main(String[] args) {
---
+++ rule-engine/src/main/java/com/learning/rule_engine/model/Rule.java
@@ -0,0 +1,53 @@
+package com.learning.rule_engine.model;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "rules")
+@Getter
+@Setter
+public class Rule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private Long policyId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private String conditionJson;
+
+    @Column(nullable = false)
+    private String actionJson;
+
+    @Column(nullable = false)
+    private Integer priority;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
